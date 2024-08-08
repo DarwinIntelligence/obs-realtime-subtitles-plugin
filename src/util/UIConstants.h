684a55351e755abd0c08f6a794483f8c1259b3b0
Rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2024-08-08 17:31:51 +0900

    OBS-23 - Add all the labels for display

--- src/util/UIConstants.h
+++ src/util/UIConstants.h
@@ -3,5 +3,11 @@
 #include <obs-module.h>
 
 #define T_(s)                   obs_module_text(s)
-#define T_WINDOW_TITLE          T_("mainwindow_title")
-#define T_AUDIO_SOURCE_LANGUAGE T_("audio_source_language")
+#define T_WINDOW_TITLE             T_("mainwindow_title")
+#define T_AUDIO_SOURCE_LABEL       T_("audio_source_label")
+#define T_SOURCE_LANGUAGE_LABEL    T_("source_language_label")
+#define T_TRANSLATE_LANGUAGE_LABEL T_("translate_language_label")
+#define T_CAPTIONS_VIEW_LABEL      T_("captions_view_label")
+#define T_PROFANITY_FILTER_LABEL   T_("profanity_filter_label")
+#define T_OUTPUT_TO_LABEL          T_("output_to_label")
+#define T_CAPTION_TIMEOUT_LABEL    T_("caption_timeout_label")
--- src/window/MainWindow.cpp
+++ src/window/MainWindow.cpp
@@ -6,7 +6,13 @@ MainWindow::MainWindow(): QWidget(), ui(new Ui::MainWindow)
 	ui->setupUi(this);
 	setVisible(false);
     setWindowTitle(QString::fromUtf8(T_WINDOW_TITLE));
-    ui->audio_source_label->setText()
+    ui->audio_source_label->setText(QString::fromUtf8(T_AUDIO_SOURCE_LABEL));
+    ui->source_language_label->setText(QString::fromUtf8(T_SOURCE_LANGUAGE_LABEL));
+    ui->translate_language_label->setText(QString::fromUtf8(T_TRANSLATE_LANGUAGE_LABEL));
+    ui->captions_view_label->setText(QString::fromUtf8(T_CAPTIONS_VIEW_LABEL));
+    ui->profanity_filter_label->setText(QString::fromUtf8(T_PROFANITY_FILTER_LABEL));
+    ui->output_to_label->setText(QString::fromUtf8(T_OUTPUT_TO_LABEL));
+    ui->caption_timeout_label->setText(QString::fromUtf8(T_CAPTION_TIMEOUT_LABEL));
 }
 
 void MainWindow::show_self()
